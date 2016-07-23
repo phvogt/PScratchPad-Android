a513f7f73fd1cb0bb9d4a52cc537bb6888e8a8e0
phvogt <phvogt@users.noreply.github.com>
2016-07-23 18:34:00 +0200

    improved preference, changed layout / button size

--- src/com/github/phvogt/pscratchpad/android/SettingsActivity.java
+++ src/com/github/phvogt/pscratchpad/android/SettingsActivity.java
@@ -1,8 +1,16 @@
 package com.github.phvogt.pscratchpad.android;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.MultiSelectListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.text.InputType;
 
 /**
  * Activity to settings.
@@ -22,7 +30,7 @@ public class SettingsActivity extends Activity {
     /**
      * Fragment for user settings.
      */
-    public static class SettingsFragment extends PreferenceFragment {
+    public static class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
 	/**
 	 * {@inheritDoc}
@@ -33,6 +41,70 @@ public class SettingsActivity extends Activity {
 
 	    // Load the preferences from an XML resource
 	    addPreferencesFromResource(R.xml.user_prefs);
+	    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	    initSummary(getPreferenceScreen());
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+	    updatePrefSummary(findPreference(key));
+	}
+
+	/**
+	 * Set value of all summaries.
+	 * 
+	 * @param p
+	 *            preference
+	 */
+	private void initSummary(final Preference p) {
+	    if (p instanceof PreferenceGroup) {
+		final PreferenceGroup pGrp = (PreferenceGroup) p;
+		for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+		    initSummary(pGrp.getPreference(i));
+		}
+	    } else {
+		updatePrefSummary(p);
+	    }
+	}
+
+	/**
+	 * Update the summary of the preference.
+	 * 
+	 * @param p
+	 *            preference
+	 */
+	private void updatePrefSummary(final Preference p) {
+	    if (p instanceof ListPreference) {
+		final ListPreference listPref = (ListPreference) p;
+		p.setSummary(listPref.getEntry());
+	    }
+	    if (p instanceof EditTextPreference) {
+		final EditTextPreference editTextPref = (EditTextPreference) p;
+		if ((((EditTextPreference) p).getEditText().getInputType()
+			& InputType.TYPE_TEXT_VARIATION_PASSWORD) != 0) {
+		    final String value = editTextPref.getText();
+		    String summary = getResources().getString(R.string.label_settings_summary_password);
+		    if (value == null || "".equals(value)) {
+			summary = getResources().getString(R.string.label_settings_summary_empty);
+		    }
+		    p.setSummary(summary);
+		} else {
+		    final String value = editTextPref.getText();
+		    String summary = value;
+		    if (value == null || "".equals(value)) {
+			summary = getResources().getString(R.string.label_settings_summary_empty);
+		    }
+		    p.setSummary(summary);
+		}
+	    }
+	    if (p instanceof MultiSelectListPreference) {
+		final EditTextPreference editTextPref = (EditTextPreference) p;
+		p.setSummary(editTextPref.getText());
+	    }
+	}
+
     }
 }
